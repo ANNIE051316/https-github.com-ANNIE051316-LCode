78a41700fdc0a79dfee0153a6ee6a5791290f88e
ANNIE051316 <anitahanq@gmail.com>
2016-06-29 12:47:11 -0700

    [80. Remove Duplicates from Sorted Array II][Runtime Error]committed by LeetCode Extension

--- 80-Remove-Duplicates-from-Sorted-Array-II/solution.java
+++ 80-Remove-Duplicates-from-Sorted-Array-II/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public int removeDuplicates(int[] nums) {
         int i = 0; 
         for(int num : nums) {
-            if(i == 0 ||  num != nums[i - 1]) {
+            if(i == 0 ||  num != nums[i - 2]) {
                 nums[++i] = num;
             }
         }
