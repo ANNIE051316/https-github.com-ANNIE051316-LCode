2d11af3e8a9c5acfd0934eed878f70304526254e
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-06-30 14:19:06 -0700

    [370. Range Addition][Compile Error]committed by LeetCode Extension

---
+++ 370-Range-Addition/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int[] getModifiedArray(int length, int[][] updates) {
+        int[]  res = new int[length];
+        for(int[] update :　updates) {
+            if(update[0] > 0) {
+                res[update[0] - 1] -= update[2];
+            }
+            res[update[1]] += update[2];
+        }
+        
+        for(int i = length - 2; i >= 0; i--) {
+            res[i] += res[i + 1];
+        }
+        return res;
+    }
+}
