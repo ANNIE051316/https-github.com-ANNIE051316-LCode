efc5192b0840e06e03404cdc27b7653b03a3b3b8
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-01 07:12:26 -0700

    [361. Bomb Enemy][Accepted]committed by LeetCode Extension

--- 361-Bomb-Enemy/solution.java
+++ 361-Bomb-Enemy/solution.java
@@ -1,5 +1,8 @@
 public class Solution {
     public int maxKilledEnemies(char[][] grid) {
+        if(grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
         int[] colCounts = new int[grid[0].length];
         int res = 0;
         for(int i = 0; i < grid.length; i++) {
