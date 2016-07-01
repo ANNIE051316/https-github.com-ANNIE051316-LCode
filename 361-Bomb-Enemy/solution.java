d222a170fd7cf38d5dcf73ef081db43add2ed3c9
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-01 07:11:33 -0700

    [361. Bomb Enemy][Runtime Error]committed by LeetCode Extension

--- 361-Bomb-Enemy/solution.java
+++ 361-Bomb-Enemy/solution.java
@@ -1,57 +1,34 @@
 public class Solution {
     public int maxKilledEnemies(char[][] grid) {
-        if(grid == null || grid.length == 0 || grid[0].length == 0) {
-            return 0;
-        }
-        int[][] counts = new int[grid.length][grid[0].length];
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++) {
-                if(grid[i][j] == 'E') {
-                    updateHelper(grid, i, j, counts);
-                }
-            }
-        }
+        int[] colCounts = new int[grid[0].length];
         int res = 0;
         for(int i = 0; i < grid.length; i++) {
+            int rowCount = 0;
             for(int j = 0; j < grid[0].length; j++) {
-                res = Math.max(res, counts[i][j]);
+                if(j == 0 || grid[i][j - 1] == 'W') {
+                    rowCount = 0;
+                    for(int k = j; k < grid[0].length && grid[i][k] != 'W'; k++) {
+                        if(grid[i][k] == 'E') {
+                            rowCount++;
+                        }
+                    }
+                }
+                
+                if(i == 0 || grid[i - 1][j] == 'W') {
+                    colCounts[j] = 0;
+                    for(int k = i; k < grid.length && grid[k][j] != 'W'; k++) {
+                        if(grid[k][j] == 'E') {
+                            colCounts[j]++;
+                        }
+                    }
+                }
+                
+                if(grid[i][j] == '0') {
+                    res = Math.max(res, colCounts[j] + rowCount);
+                }
             }
         }
         
         return res;
-    }
-    
-    public void updateHelper(char[][] grid, int row, int col, int[][] counts) {
-        int i = row;
-        while(i > 0 && grid[i - 1][col] != 'W') {
-            if(grid[i - 1][col] == '0') {
-                counts[i - 1][col] += 1; 
-            }
-            i--;
-        }
-        
-        i = row;
-         while(i < grid.length - 1 && grid[i + 1][col] != 'W') {
-            if(grid[i + 1][col] == '0') {
-                counts[i + 1][col] += 1; 
-            }
-            i++;
-        }
-        
-        i = col;
-        while(i > 0 && grid[row][i - 1] != 'W') {
-            if(grid[row][i - 1] == '0') {
-                counts[row][i - 1] += 1;
-            }
-            i--;
-        }
-        
-        i = col;
-        while(i < grid[0].length - 1 && grid[row][i + 1] != 'W') {
-            if(grid[row][i + 1] == '0') {
-                counts[row][i + 1] += 1;
-            }
-            i++;
-        } 
     }
 }
