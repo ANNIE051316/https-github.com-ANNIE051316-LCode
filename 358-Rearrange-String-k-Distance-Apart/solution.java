1475f5d366f292ad18a5da05f4e41a2b1a695dcd
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-01 10:35:55 -0700

    [358. Rearrange String k Distance Apart][Accepted]committed by LeetCode Extension

--- 358-Rearrange-String-k-Distance-Apart/solution.java
+++ 358-Rearrange-String-k-Distance-Apart/solution.java
@@ -7,8 +7,6 @@ public class Solution {
         
         int len = str.length();
         int[] count = new int[26];
-        int[] lastIndex = new int[26];
-        boolean[] added = new boolean[26];
         for(int i = 0; i < len; i++) {
             char c = str.charAt(i);
             count[c - 'a']++;
@@ -17,7 +15,12 @@ public class Solution {
         PriorityQueue<Pair<Character, Integer>> pq = new PriorityQueue<Pair<Character, Integer>>(11, new Comparator<Pair>() {
             @Override
             public int compare(Pair p1, Pair p2){
-                return (int)p2.getValue() - (int)p1.getValue();
+                int v1 = (int)p1.getValue();
+                int v2 = (int)p2.getValue();
+                if(v1 == v2) {
+                    return (char)p1.getKey() - (char)p2.getKey();
+                } 
+                return v2 - v1;
             } 
             });
             
@@ -25,28 +28,25 @@ public class Solution {
             if(count[i] != 0) {
                 Pair<Character, Integer> tmp = new Pair<Character, Integer>((char)(i + 'a'), count[i]);
                 pq.offer(tmp);
-                added[i] = true;
             }
         }
         
         StringBuilder sb = new StringBuilder();
-        int index = 0;
-        while(index < len && !pq.isEmpty()) {
+        Queue<Pair> queue = new LinkedList<Pair>();
+        while(!pq.isEmpty()) {
             Pair cur = pq.poll();
             char c = (char)cur.getKey();
-            added[c - 'a'] = false;
             sb.append(c);
-            lastIndex[c - 'a'] = index + 1;
-            count[c - 'a']--;
-            index++;
-            for(int i = 0; i < 26; i++) {
-                if(count[i] != 0 && !added[i] && (lastIndex[i] == 0 || index - lastIndex[i] >= k - 1)) {
-                    pq.offer(new Pair((char)(i + 'a'), count[i]));
-                    added[i] = true;
-                }
+            queue.offer(new Pair(c, --count[c - 'a']));
+            
+            if(queue.size() < k) {
+                continue;
+            }
+            Pair head = queue.poll();
+            if((int)head.getValue() != 0) {
+                pq.offer(head);
             }
         }
-        
         
         return sb.length() != len ? "" : sb.toString();
     }
