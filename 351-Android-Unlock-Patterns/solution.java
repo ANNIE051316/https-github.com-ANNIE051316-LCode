878359a4314ffe4a745b7cf2b7d4c396438f1e9c
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-02 11:52:08 -0700

    [351. Android Unlock Patterns][Accepted]committed by LeetCode Extension

--- 351-Android-Unlock-Patterns/solution.java
+++ 351-Android-Unlock-Patterns/solution.java
@@ -1,61 +1,48 @@
 public class Solution {
     public int numberOfPatterns(int m, int n) {
-        List<List<Integer>> res = new ArrayList();
-        for(int i = m; i <= n ; i++) {
-            helper(0, new ArrayList<Integer>(), i, new HashSet<Integer>(), res);
-        } 
-        
-        return res.size();
-    }
-    
-    public void helper(int cur, List<Integer> path, int numToAdd, Set<Integer> visited, List<List<Integer>> res) {
-        if(numToAdd == 0) {
-            res.add(new ArrayList<Integer>(path));
-            return;
+        int res = 0;
+        for(int i = m; i <= n; i++) {
+            res += dfs(0, 1, new boolean[9], i - 1) * 4;
+            res += dfs(0, 2, new boolean[9], i - 1) * 4;
+            res += dfs(0, 5, new boolean[9], i - 1);
         }
-        for(int i = 1; i <= 9; i++) {
-            if(visited.contains(i)) {
-                continue;
-            }
-            
-            if(cur == 1 && (i == 3 && !visited.contains(2) || i == 9 && !visited.contains(5) || i == 7 && !visited.contains(4))) {
-                continue;
-            }
-            
-            if(cur == 2 && i == 8 && !visited.contains(5)) {
-                continue;
-            }
-            
-            if(cur == 3 && (i == 1 && !visited.contains(2) || i == 7 && !visited.contains(5) || i == 9 && !visited.contains(6))) {
-                continue;
-            }
-            
-            if(cur == 4 && i == 6 && !visited.contains(5)) {
-                continue;
-            }
-            
-            if(cur == 6 && i == 4 && !visited.contains(5)) {
-                continue;
-            }
-            
-            if(cur == 7 && (i == 1 && !visited.contains(4) || i == 3 && !visited.contains(5) || i == 9 && !visited.contains(8))) {
-                continue;
-            }
-            
-            if(cur == 8 && i == 2 && !visited.contains(5)) {
-                continue;
-            }
-            
-            if(cur == 9 && (i == 1 && !visited.contains(5) || i == 3 && !visited.contains(6) || i == 7 && !visited.contains(8))) {
-                continue;
-            }
-            
-            path.add(i);
-            visited.add(i);
-            helper(i, path, numToAdd - 1, visited, res);
-            visited.remove(i);
-            path.remove(path.size() - 1);
-         }
+        
+        return res;
     }
     
+    public int dfs(int last, int cur, boolean[] visited, int numToAdd) {
+        if(numToAdd == 0) {
+            return 1;
+        }
+        visited[cur - 1] = true;
+        int res = 0;
+        for(int i = 1; i <= 9; i++) {
+            if(isValid(visited, i, cur)) {
+                res += dfs(cur, i, visited, numToAdd -1);
+            }
+        }
+        visited[cur - 1] = false;
+        return res;
+    }
+    
+    
+    public boolean isValid(boolean[] visited, int cur, int last) {
+        if(visited[cur - 1]) {
+            return false;
+        }
+        int min = Math.min(cur, last);
+        int max = Math.max(cur, last);
+        int mid = (min + max) / 2;
+        if((min == 1 || min == 4 || min == 7) && max == min + 2) {
+            return visited[mid - 1];
+        }
+        
+        if(min < 4 && max == min + 6) {
+            return visited[mid - 1];
+        }
+        if(min == 1 && max == 9 || min == 3 && max == 7) {
+            return visited[mid - 1];
+        }
+        return true;
+    }
 }
