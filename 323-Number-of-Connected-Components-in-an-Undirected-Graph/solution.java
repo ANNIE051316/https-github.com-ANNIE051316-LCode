a1ed052993ea5709a1e53b6226025a78d52e9ff0
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-02 13:55:18 -0700

    [323. Number of Connected Components in an Undirected Graph][Runtime Error]committed by LeetCode Extension

---
+++ 323-Number-of-Connected-Components-in-an-Undirected-Graph/solution.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public int countComponents(int n, int[][] edges) {
+        int count = 0;
+        Set<Integer>[] adj = new HashSet[n];
+        boolean[] visited = new boolean[n];
+        for(int[] edge : edges) {
+            if(adj[edge[0]] == null) {
+                adj[edge[0]] = new HashSet<Integer>();
+            }
+            
+            if(adj[edge[1]] == null) {
+                adj[edge[1]] = new HashSet<Integer>();
+            }
+            
+            adj[edge[0]].add(edge[1]);
+            adj[edge[1]].add(edge[0]);
+        }
+        
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                count++;
+                dfs(i, visited, adj);
+            }
+        }
+        
+        return count;
+    }
+    
+    public void dfs(int index, boolean[] visited, Set<Integer>[] adj) {
+        visited[index] = true;
+        
+        for(Integer i : adj[index]) {
+            if(!visited[i]) {
+                dfs(i, visited, adj);
+            }
+        }
+    
+    }
+}
