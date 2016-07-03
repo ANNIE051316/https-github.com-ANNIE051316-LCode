e0279bfff4190d4249992ea48f7e02ec0a525fc3
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-03 00:21:49 -0700

    [311. Sparse Matrix Multiplication][Accepted]committed by LeetCode Extension

--- 311-Sparse-Matrix-Multiplication/solution.java
+++ 311-Sparse-Matrix-Multiplication/solution.java
@@ -3,21 +3,14 @@ public class Solution {
         int[][] res = new int[A.length][B[0].length];
         
         for(int i = 0; i < A.length; i++) {
-            Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-            for(int j = 0; j < A[0].length; j++) {
-                if(A[i][j] != 0) {
-                    map.put(j, A[i][j]);
+            for(int k = 0; k < A[0].length; k++) {
+                if(A[i][k] != 0) {
+                    for(int j = 0; j < B[0].length; j++) {
+                        if(B[k][j] != 0) {
+                            res[i][j] += A[i][k] * B[k][j];
+                        }
+                    }
                 }
-            }
-            
-            for(int j = 0; j < B[0].length; j++) {
-                int tmp = 0;
-                for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-                    int col = entry.getKey();
-                    int value = entry.getValue();
-                    tmp += value * B[col][j];
-                }
-                res[i][j] = tmp;
             }
         }
         
