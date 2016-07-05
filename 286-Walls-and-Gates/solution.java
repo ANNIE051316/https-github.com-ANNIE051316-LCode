64ed0a2be61354613f7cdd0821b9c9ed4784e2e3
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-04 18:21:04 -0700

    [286. Walls and Gates][Accepted]committed by LeetCode Extension

--- 286-Walls-and-Gates/solution.java
+++ 286-Walls-and-Gates/solution.java
@@ -3,7 +3,7 @@ public class Solution {
         if(rooms == null || rooms.length == 0 || rooms[0].length == 0) {
             return;
         }
-      //  boolean[][] visited = new boolean[rooms.length][rooms[0].length];
+        int[][] directions = {{1, 0}, {-1, 0}, {0, 1}, {0, - 1}};
         
         Queue<Integer> queue = new LinkedList<Integer>();
         for(int i = 0; i < rooms.length; i++) {
@@ -11,46 +11,24 @@ public class Solution {
                 if(rooms[i][j] == 0) {
                     queue.offer(i);
                     queue.offer(j);
-          //          visited[i][j] = true;
                 }
             }
         }
-        int step = 1;
         while(!queue.isEmpty()) {
-            int sz = queue.size() >> 1;
-            for(int i = 0; i < sz; i++) {
-                int row = queue.poll();
-                int col = queue.poll();
-                if(row > 0 && rooms[row - 1][col] == Integer.MAX_VALUE) {
-          //          visited[row - 1][col] = true;
-                    rooms[row - 1][col] = step;
-                    queue.offer(row - 1);
-                    queue.offer(col);
-                }
-                if(row < rooms.length - 1 && rooms[row + 1][col] == Integer.MAX_VALUE) {
-              //      visited[row + 1][col] = true;
-                    rooms[row + 1][col] = step;
-                    queue.offer(row + 1);
-                    queue.offer(col);
+            int row = queue.poll();
+            int col = queue.poll();
+            for(int[] direction : directions) {
+                int r = row + direction[0];
+                int c = col + direction[1];
+                if(r < 0 || r >= rooms.length || c < 0 || c >= rooms[0].length || rooms[r][c] != Integer.MAX_VALUE) {
+                    continue;
                 }
                 
-                if(col > 0 && rooms[row][col - 1] == Integer.MAX_VALUE) {
-                //    visited[row][col - 1] = true;
-                    rooms[row][col - 1] = step;
-                    queue.offer(row);
-                    queue.offer(col - 1);
-                }
-                
-                if(col < rooms[0].length - 1 && rooms[row][col + 1] == Integer.MAX_VALUE) {
-                //    visited[row][col + 1] = true;
-                    rooms[row][col + 1] = step;
-                    queue.offer(row);
-                    queue.offer(col + 1);
-                }
+                queue.offer(r);
+                queue.offer(c);
+                rooms[r][c] = rooms[row][col] + 1;
             }
-            step++;
         }
-        
     
     }
 }
