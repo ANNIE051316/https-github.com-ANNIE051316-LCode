62600633cb80315b67cab715070ad1f440a712b0
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-04 17:08:54 -0700

    [293. Flip Game][Accepted]committed by LeetCode Extension

--- 293-Flip-Game/solution.java
+++ 293-Flip-Game/solution.java
@@ -1,22 +1,9 @@
 public class Solution {
     public List<String> generatePossibleNextMoves(String s) {
         List<String> res = new ArrayList<String>();
-        if(s == null) {
-            return res;
-        }
-        
-        char[] chars = s.toCharArray();
-        
-        for(int i = 0; i < chars.length - 1; i++) {
-            if(chars[i] == '-' || chars[i + 1] == '-') {
-                continue;
-            }
-            
-            chars[i] = '-';
-            chars[i + 1] = '-';
-            res.add(String.valueOf(chars));
-            chars[i] = '+';
-            chars[i + 1] = '+';
+        int len = s.length();
+        for(int i = -1; (i = s.indexOf("++", i + 1)) >= 0;) {
+            res.add(s.substring(0, i) + "--" + s.substring(i + 2));
         }
         
         return res;
