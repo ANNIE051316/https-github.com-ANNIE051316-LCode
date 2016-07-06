f27e6e9bf56790ef714331acb573e8821ffa7dee
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-06 15:33:55 -0700

    [271. Encode and Decode Strings][Compile Error]committed by LeetCode Extension

---
+++ 271-Encode-and-Decode-Strings/solution.java
@@ -0,0 +1,37 @@
+public class Codec {
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuidler sb = new StringBuilder();
+        for(String s : strs) {
+            sb.append(s.length() + "/" + s);
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> res = new ArrayList<String>();
+        int len = s.length();
+        int curlen = 0;
+        int index = 0;
+        while(index < len) {
+            char c = s.charAt(index);
+            if(c != '/') {
+                curlen = curlen * 10 + c - '0';
+            }
+            else {
+                
+                res.add(s.substring(index + 1, index + curlen + 2));
+                index = index + curlen + 2;
+                curlen = 0;
+            }
+        }
+        
+        return res;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
