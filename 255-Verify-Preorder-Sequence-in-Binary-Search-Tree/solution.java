206007f7da2898d077063de94bb6f7de5c481cd5
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-12 16:17:07 -0700

    [255. Verify Preorder Sequence in Binary Search Tree][Accepted]committed by LeetCode Extension

--- 255-Verify-Preorder-Sequence-in-Binary-Search-Tree/solution.java
+++ 255-Verify-Preorder-Sequence-in-Binary-Search-Tree/solution.java
@@ -1,24 +1,18 @@
 public class Solution {
     public boolean verifyPreorder(int[] preorder) {
-        return valid(preorder, 0, preorder.length - 1, Long.MIN_VALUE, Long.MAX_VALUE);       
-    }
-    
-    public boolean valid(int[] preorder, int start, int end, long min, long max) {
-        if(start > end) {
-            return true;
-        }
+        int low = Integer.MIN_VALUE;
+        Stack<Integer> stack = new Stack<Integer>();
         
-        if(preorder[start] <= min || preorder[start] >= max) {
-            return false;
-        }
-        int index = -1;
-        for(int i = start; i < end; i++) {
-            if(preorder[i + 1] > preorder[start]) {
-                index = i;
-                break;
+        for(int p : preorder) {
+            if(p < low) {
+                return false;
             }
+            while(!stack.isEmpty() && stack.peek() < p) {
+                low = stack.pop();
+            }
+            stack.push(p);
         }
         
-        return valid(preorder, start + 1, index, min, preorder[start]) && valid(preorder, Math.max(index + 1, start + 1), end, preorder[start], max); 
+        return true;
     }
 }
