72a6f06b7a434583a0c1aebd71a6e934051c749c
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-16 13:39:58 -0700

    [354. Russian Doll Envelopes][Accepted]committed by LeetCode Extension

--- 354-Russian-Doll-Envelopes/solution.java
+++ 354-Russian-Doll-Envelopes/solution.java
@@ -5,25 +5,47 @@ public class Solution {
         }
         
         Arrays.sort(envelopes, new Comparator<int[]>() {
-            @Override 
-            public int compare(int[] e1, int[] e2) {
+            @Override
+            public int compare(int[] e1, int e2[]) {
+                if(e1[0] == e2[0]) {
+                    return e2[1] - e1[1];
+                }
                 return e1[0] - e2[0];
             }
         });
         
-        int[] dp = new int[envelopes.length];
-        dp[0] = 1;
-        int res = 1;
+        int len = 1;
+        int[] sorted = new int[envelopes.length];
+        sorted[0] = envelopes[0][1];
+        
         for(int i = 1; i < envelopes.length; i++) {
-            dp[i] = 1;
-            for(int j = i - 1; j >= 0; j--) {
-                if(envelopes[j][0] < envelopes[i][0] && envelopes[j][1] < envelopes[i][1]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+            if(envelopes[i][1] > sorted[len - 1]) {
+                sorted[len++] = envelopes[i][1];
             }
-            res = Math.max(res, dp[i]);
+            else {
+                int pos = find(sorted, 0, len - 1, envelopes[i][1]);
+                sorted[pos] = envelopes[i][1];
+            }
         }
         
-        return res;
+        return len;
     }
+    
+    public int find(int[] sorted, int left, int right, int target) {
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(sorted[mid] == target) {
+                return mid;
+            }
+            else if(sorted[mid] > target) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+    
 }
