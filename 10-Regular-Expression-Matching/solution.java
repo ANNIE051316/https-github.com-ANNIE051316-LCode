1d328ec9f7a3be3440585539b8090ac6dacf2305
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-16 14:43:09 -0700

    [10. Regular Expression Matching][Accepted]committed by LeetCode Extension

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -1,46 +1,30 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        if(p == null || p.length() == 0) {
-            return s == null || s.length() == 0;
+        int slen = s.length();
+        int plen = p.length();
+        if(plen == 0) {
+            return slen == 0;
         }
         
-        return backtracking(s, 0, s.length() - 1, p, 0, p.length() - 1); 
-    }
-    
-    public boolean backtracking(String s, int start, int end, String p, int pstart, int pend) {
-        if(start > end) {
-            if(pstart > pend) {
+        if(plen == 1) {
+            return (slen == 1 && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0)));
+        }
+        
+        if(p.charAt(1) == '*') {
+            if(isMatch(s, p.substring(2))) {
                 return true;
             }
             else {
-                while(pstart <= pend) {
-                    if(p.charAt(pstart) != '*') {
-                        return false;
-                    }
-                    pstart++;
+                if(slen > 0 && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0))) {
+                    return isMatch(s.substring(1), p);
                 }
-                return true;
+                else {
+                    return false;
+                }
             }
         }
-            
-            if(pstart > pend) {
-                return false;
-            }
-            char c = s.charAt(start);
-            char t = p.charAt(pstart);
-            if(c == t || t == '.') {
-                return backtracking(s, start + 1, end, p, pstart + 1, pend);
-            }
-            if(t == '*') {
-                return backtracking(s, start, end, p, pstart + 1, pend) || backtracking(s, start, end, p, pstart - 1, pend);
-            }
-            else {
-                if(pstart < pend && p.charAt(pstart + 1) == '*') {
-                    return backtracking(s, start, end, p, pstart + 1, pend);
-                }       
-                return false;
-            }
-            
-            
+        else {
+            return (slen > 0 && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0)) && isMatch(s.substring(1), p.substring(1)));
+        }
     }
 }
