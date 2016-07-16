76958f2512e122e32949ce68b89e588b408860da
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-16 13:05:57 -0700

    [354. Russian Doll Envelopes][Accepted]committed by LeetCode Extension

---
+++ 354-Russian-Doll-Envelopes/solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        if(envelopes == null || envelopes.length == 0 || envelopes[0].length == 0) {
+            return 0;
+        }
+        
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            @Override 
+            public int compare(int[] e1, int[] e2) {
+                return e1[0] - e2[0];
+            }
+        });
+        
+        int[] dp = new int[envelopes.length];
+        dp[0] = 1;
+        int res = 1;
+        for(int i = 1; i < envelopes.length; i++) {
+            int tmpmax = 0;
+            for(int j = i - 1; j >= 0; j--) {
+                if(envelopes[j][0] < envelopes[i][0] && envelopes[j][1] < envelopes[i][1]) {
+                    tmpmax = Math.max(tmpmax, dp[j]);
+                }
+            }
+            dp[i] = 1 + tmpmax;
+            res = Math.max(res, dp[i]);
+        }
+        
+        return res;
+    }
+}
