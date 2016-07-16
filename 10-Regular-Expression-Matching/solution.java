a4ebb7e9764fe8e7b79b50786ef8f92293b91b42
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-16 16:15:18 -0700

    [10. Regular Expression Matching][Accepted]committed by LeetCode Extension

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -4,19 +4,23 @@ public class Solution {
         int plen = p.length();
         boolean[][] dp = new boolean[slen + 1][plen + 1];
         dp[0][0] = true;
-        char[] schar = s.toCharArray();
-        char[] pchar = p.toCharArray();
-        for(int i = 0; i <= slen; i++) {
+        for(int j = 1; j < plen && p.charAt(j) == '*'; j += 2) {
+            dp[0][j] = true;
+            dp[0][j + 1] = true;
+        }
+        
+        for(int i = 1; i <= slen; i++) {
+            char c = s.charAt(i - 1);
             for(int j = 1; j <= plen; j++) {
-                if(pchar[j - 1] != '*') {
-                    dp[i][j] = i > 0 && dp[i - 1][j - 1] && (pchar[j - 1] == '.' || pchar[j - 1] == schar[i - 1]);
+                char t = p.charAt(j - 1);
+                if(t != '*') {
+                    dp[i][j] = dp[i - 1][j - 1] && (t == '.' || t == c);
                 }
                 else {
-                    dp[i][j] = dp[i][j - 2] || (i > 0 && dp[i - 1][j] && (schar[i - 1] == pchar[j - 2] || pchar[j - 2] == '.'));
+                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (c == p.charAt(j - 2) || p.charAt(j - 2) == '.'));
                 }
             }
-         }
-         
-         return dp[slen][plen];
+        }
+        return dp[slen][plen];
     }
 }
