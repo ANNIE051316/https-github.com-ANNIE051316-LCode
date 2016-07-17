187dd5e378bb799ad874eba5a3ef47beeb6f103d
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-17 14:30:38 -0700

    [59. Spiral Matrix II][Accepted]committed by LeetCode Extension

---
+++ 59-Spiral-Matrix-II/solution.java
@@ -0,0 +1,45 @@
+public class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        int startx = 0, endx = n - 1, starty = 0, endy = n - 1;
+        int cur = 1;
+        int total = n * n;
+        while(cur <= total) {
+            // top
+            for(int i = starty; i <= endy; i++) {
+                matrix[startx][i] = cur;
+                cur++;
+            }
+            if(++startx > endx) {
+                break;
+            }
+            
+            for(int i = startx; i <= endx; i++) {
+                matrix[i][endy] = cur;
+                cur++;
+            }
+            
+            if(--endy < starty) {
+                break;
+            }
+            
+            for(int i = endy; i >= starty; i--) {
+                matrix[endx][i] = cur;
+                cur++;
+            }
+            if(--endx < startx) {
+                break;
+            }
+            
+            for(int i = endx; i >= startx; i--) {
+                matrix[i][starty] = cur;
+                cur++;
+            }
+            if(++starty > endy) {
+                break;
+            }
+        }
+        
+        return matrix;
+    }
+}
