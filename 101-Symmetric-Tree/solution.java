1f14499eaf6749e961db53771917b1b00205f4b5
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-18 00:11:26 -0700

    [101. Symmetric Tree][Runtime Error]committed by LeetCode Extension

--- 101-Symmetric-Tree/solution.java
+++ 101-Symmetric-Tree/solution.java
@@ -9,21 +9,37 @@
  */
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root == null) {
+        if(root == null || (root.left == null && root.right == null)) {
             return true;
         }
         
-        return isSym(root.left, root.right);
-    }
-    
-    public boolean isSym(TreeNode root1, TreeNode root2) {
-        if(root1 == null) {
-            return root2 == null;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+        while(!queue.isEmpty()) {
+            TreeNode node1 = queue.poll();
+            TreeNode node2 = queue.poll();
+            if(node1.val != node2.val) {
+                return false;
+            }
+            if(node1.left != null && node2.right != null) {
+                queue.offer(node1.left);
+                queue.offer(node2.right);
+            }
+            else if(node1.left != null || node2.right != null) {
+                return false;
+            }
+            
+            if(node1.right != null && node2.left != null) {
+                queue.offer(node1.right);
+                queue.offer(node2.left);
+            }
+            else if(node1.right != null || node2.left != null) {
+                return false;
+            }
         }
-        if(root2 == null || root1.val != root2.val) {
-            return false;
-        }
-        return isSym(root1.left, root2.right) && isSym(root1.right, root2.left);
+        
+        return true;
         
     }
 }
