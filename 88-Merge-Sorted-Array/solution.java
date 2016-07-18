24a49284814581a46823ddafbb1dc99343044a29
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-17 22:44:09 -0700

    [88. Merge Sorted Array][Runtime Error]committed by LeetCode Extension

--- 88-Merge-Sorted-Array/solution.java
+++ 88-Merge-Sorted-Array/solution.java
@@ -1,6 +1,6 @@
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        for(int i = m + n - 1, j = n - 1, k = m - 1; j >= 0 && i >= 0; i++) {
+        for(int i = m + n - 1, j = n - 1, k = m - 1; j >= 0 && i >= 0; i--) {
             if(k < 0 || nums2[j] >= nums1[k]) {
                 nums1[i] = nums2[j--];
             }
