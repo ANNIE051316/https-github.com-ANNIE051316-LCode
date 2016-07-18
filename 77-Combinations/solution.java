8790a0a315394742ac40bde2bdf8332c63e9e49e
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-17 18:46:25 -0700

    [77. Combinations][Accepted]committed by LeetCode Extension

---
+++ 77-Combinations/solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        combinehelper(1, n, new ArrayList<Integer>(), k, res);
+        return res;
+    }
+    
+    public void combinehelper(int start, int end, List<Integer> path, int numneeded, List<List<Integer>> res) {
+        if(numneeded == 0) {
+            res.add(new ArrayList<Integer>(path));
+            return;
+        }
+        
+        for(int i = start; i <= end; i++) {
+            path.add(i);
+            combinehelper(i + 1, end, path, numneeded - 1, res);
+            path.remove(path.size() - 1);
+        }
+    }
+}
