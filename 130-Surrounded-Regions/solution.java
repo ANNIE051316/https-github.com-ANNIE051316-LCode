3684676f91653a435434632c60f015dae4400ae8
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-19 01:46:35 -0700

    [130. Surrounded Regions][Accepted]committed by LeetCode Extension

--- 130-Surrounded-Regions/solution.java
+++ 130-Surrounded-Regions/solution.java
@@ -3,29 +3,30 @@ public class Solution {
         if(board == null || board.length == 0 || board[0].length == 0) {
             return;
         }
-        
-        for(int i = 0; i < board.length; i++) {
+        int m = board.length;
+        int n = board[0].length;
+        for(int i = 0; i < m; i++) {
             if(board[i][0] == 'O') {
                 helper(board, i, 0);
             }
             
-            if(board[i][board[0].length - 1] == 'O') {
-                helper(board, i, board[0].length - 1);
+            if(board[i][n - 1] == 'O') {
+                helper(board, i, n - 1);
             }
         }
         
-        for(int j = 0; j < board[0].length; j++) {
+        for(int j = 0; j < n; j++) {
             if(board[0][j] == 'O') {
                 helper(board, 0, j);
             }
             
-            if(board[board.length - 1][j] == 'O') {
-                helper(board, board.length - 1, j);
+            if(board[m - 1][j] == 'O') {
+                helper(board, m - 1, j);
             }
         }
         
-        for(int i = 0; i < board.length; i++) {
-            for(int j = 0; j < board[0].length; j++) {
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
                 if(board[i][j] == 'O') {
                     board[i][j] = 'X';
                 }
@@ -42,7 +43,7 @@ public class Solution {
             helper(board, row - 1, col);
         }
         
-        if(row < board.length - 1 && board[row + 1][col] == 'O') {
+        if(row < board.length - 2 && board[row + 1][col] == 'O') {
             helper(board, row + 1, col);
         }
         
@@ -50,7 +51,7 @@ public class Solution {
             helper(board, row, col - 1);
         }
         
-        if(col < board[0].length - 1 && board[row][col + 1] == 'O') {
+        if(col < board[0].length - 2 && board[row][col + 1] == 'O') {
             helper(board, row, col + 1);
         }
     }
