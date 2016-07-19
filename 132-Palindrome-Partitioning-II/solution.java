0e6eeef293b2a1cb3e58916a08b17c1c5a265350
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-19 16:48:21 -0700

    [132. Palindrome Partitioning II][Accepted]committed by LeetCode Extension

--- 132-Palindrome-Partitioning-II/solution.java
+++ 132-Palindrome-Partitioning-II/solution.java
@@ -6,29 +6,25 @@ public class Solution {
         int len = s.length();
         int[] dp = new int[len + 1];
         
-        for(int i = 1; i <= len; i++) {
-            int min = dp[i - 1]  + 1;
-            for(int j = 1; j <= i; j++) {
-                if(dp[j - 1] + 1 < min && isValid(s.substring(j - 1, i))) {
-                    min = dp[j - 1] + 1;
-                } 
+        for(int i = 0; i < len; i++) {
+            dp[i + 1] = i + 1;
+        }
+        
+        for(int i = 0; i < len; i++) {
+            // odd palindrome
+            
+            for(int j = 0; j <= i && i + j < len && s.charAt(i - j) == s.charAt(i + j); j++) {
+                dp[i + j + 1] = Math.min(dp[i + j + 1], dp[i - j] + 1);
             }
-            dp[i] = min;
+            
+            // even palindrome
+            
+            for(int j = 0; j <= i && i + j + 1 < len && s.charAt(i - j) == s.charAt(i + j + 1); j++) {
+                dp[i + j + 2] = Math.min(dp[i + j + 2], dp[i - j] + 1);
+            }
         }
         
         return dp[len] - 1;
-    }
-    
-    public boolean isValid(String s) {
-        int left = 0, right = s.length() - 1;
-        while(left < right) {
-            if(s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
-        }
-        return true;
     }
     
 }
