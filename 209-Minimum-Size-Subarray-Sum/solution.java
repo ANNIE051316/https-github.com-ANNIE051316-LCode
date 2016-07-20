04e6096c2ec2027b1607fcea8bb86d9fe5f07479
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-20 00:41:32 -0700

    [209. Minimum Size Subarray Sum][Accepted]committed by LeetCode Extension

--- 209-Minimum-Size-Subarray-Sum/solution.java
+++ 209-Minimum-Size-Subarray-Sum/solution.java
@@ -1,20 +1,35 @@
 public class Solution {
     public int minSubArrayLen(int s, int[] nums) {
-        int res= Integer.MAX_VALUE;
-        int start = 0;
-        int cursum = 0;
+        int[] sum = new int[nums.length + 1];
         for(int i = 0; i < nums.length; i++) {
-            cursum += nums[i];
-            if(cursum < s) {
+            sum[i + 1] = sum[i] + nums[i];
+        }
+        int res = Integer.MAX_VALUE;
+        for(int i = 1; i <= nums.length; i++) {
+            if(sum[i] < s) {
                 continue;
             }
             
-            while(cursum - nums[start] >= s) {
-                cursum -= nums[start++];
-            }
-            res = Math.min(res, i - start + 1);
+            int index = binarysearch(sum[i] - s, 0, i - 1, sum);
+            res = Math.min(res, i - index);
         }
         
         return res == Integer.MAX_VALUE ? 0 : res;
+    }
+    
+    private int binarysearch(int target, int left, int right, int[] sum) {
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(sum[mid] == target) {
+                return mid;
+            }
+            else if(sum[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        return right;
     }
 }
