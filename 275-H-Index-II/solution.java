206a08f9f994a69a9a957859c7d9776999933626
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-24 18:33:25 -0700

    [275. H-Index II][Compile Error]committed by LeetCode Extension

--- 275-H-Index-II/solution.java
+++ 275-H-Index-II/solution.java
@@ -4,12 +4,21 @@ public class Solution {
             return 0;
         }
         
-        for(int i = citations.length - 1; i >= 0; i--) {
-            if(citations.length - i  <= citations[i] && (i == 0 || citations[i - 1] <= citations.length - i)) {
-                return citations.length - i;
+        int left = 0, right = citations.length - 1;
+        
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(citations[mid] == citations.length - mid) {
+                return mid;
+            else if(citations[mid] < citations.length - mid) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
             }
         }
         
-        return 0;
+        return citations.length - left;
+        
     }
 }
