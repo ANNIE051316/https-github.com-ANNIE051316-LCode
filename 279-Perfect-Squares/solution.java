f2356d50437cf1a6e6f76d3533e7eb485633ae3a
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-24 18:48:03 -0700

    [279. Perfect Squares][Runtime Error]committed by LeetCode Extension

---
+++ 279-Perfect-Squares/solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int numSquares(int n) {
+        if(n <= 0) {
+            return 0;
+        }
+        
+        int[] dp = new int[n];
+        int cur = 1;
+        for(int i = 1; i <= n; i++) {
+            if(cur * cur == i) {
+                dp[i - 1] = 1;
+                cur += 1;
+            }
+            else {
+                int tmp = Integer.MAX_VALUE;
+                for(int j = n / 2; j > 0; j--) {
+                    tmp = Math.min(tmp, dp[j - 1] + dp[i - j - 1]);
+                }
+                dp[i - 1] = tmp;
+            }
+        }
+        
+        return dp[n - 1];
+    }
+}
