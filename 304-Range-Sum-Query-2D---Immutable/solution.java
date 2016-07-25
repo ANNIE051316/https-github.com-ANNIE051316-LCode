ac6b4f33cb9117857fd00d55e2daa243a06f6e23
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-24 23:26:35 -0700

    [304. Range Sum Query 2D - Immutable][Compile Error]committed by LeetCode Extension

--- 304-Range-Sum-Query-2D---Immutable/solution.java
+++ 304-Range-Sum-Query-2D---Immutable/solution.java
@@ -1,30 +1,25 @@
 public class NumMatrix {
-    int[][] sum;
+    int[][] matrix;
     public NumMatrix(int[][] matrix) {
-        sum = new int[matrix.length][matrix[0].length];
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[0].length; j++) {
-                sum[i][j] += matrix[i][j];
-                if(j > 0) {
-                    sum[i][j] += sum[i][j - 1];
-                }
-                if(i > 0) {
-                    sum[i][j] += sum[i - 1][j];
-                }
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return;
+        }
+        this.matrix = matrix;
+        for(int i = 1; i < matrix.length; i++) {
+            for(j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] += matrix[i - 1][j];
             }
         }
+        
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return getsum(row2, col2) - getsum(row2 - 1, col2) - getsum(row2, col2 - 1) + getsum(row1 - 1, col1 - 1);
-    }
-    
-    public int getsum(int row, int col) {
-        if(row < 0 || col < 0) {
-            return 0;
+        int sum = 0;
+        for(int j = col1; j <= col2; j++) {
+            sum += matrix[row2][j] - (row1 > 0 ? matrix[row1 - 1, j] : 0);
         }
         
-        return sum[row][col];
+        return sum;
     }
 }
 
