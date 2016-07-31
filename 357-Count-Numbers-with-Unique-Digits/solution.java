be335c46b9fbbed97e0f8810a0e902a690d56a45
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-31 14:59:31 -0700

    [357. Count Numbers with Unique Digits][Accepted]committed by LeetCode Extension

--- 357-Count-Numbers-with-Unique-Digits/solution.java
+++ 357-Count-Numbers-with-Unique-Digits/solution.java
@@ -1,24 +1,18 @@
 public class Solution {
     public int countNumbersWithUniqueDigits(int n) {
-        if(n < 0 || n > 10) {
-            return 0;
-        }   
         if(n == 0) {
             return 1;
         }
-        if(n == 1) {
-            return 10;
-        }
-        int[] dp = new int[n];
-        dp[0] = 10;
-        int mul = 81;
-        int prev = 9;
-        for(int i = 1; i < n; i++) {
-            dp[i] = dp[i - 1] + mul;
-            prev--;
-            mul *= prev;
-        }
         
-        return dp[n - 1];
+        int res = 10;
+        int prev = 9;
+        int numsavailable = 9;
+        while(n > 1 & numsavailable > 0) {
+            prev = prev * numsavailable;
+            numsavailable--;
+            res += prev;
+            n--;
+        }
+        return res;
     }
 }
