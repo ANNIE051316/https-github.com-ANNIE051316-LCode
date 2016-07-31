3c39a9f5ac84ad5fc44a2cf5f55c4aed3644e9cc
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-30 18:56:30 -0700

    [315. Count of Smaller Numbers After Self][Accepted]committed by LeetCode Extension

--- 315-Count-of-Smaller-Numbers-After-Self/solution.java
+++ 315-Count-of-Smaller-Numbers-After-Self/solution.java
@@ -1,26 +1,37 @@
 public class Solution {
     public List<Integer> countSmaller(int[] nums) {
-        List<Integer> sorted = new ArrayList<Integer>();
         List<Integer> res = new ArrayList();
+        
+        int[] tmp = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(tmp);
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] = Arrays.binarySearch(tmp, nums[i]) + 1;
+        }
+        
+        int[] bit = new int[nums.length];
+        
         for(int i = nums.length - 1; i >= 0; i--) {
-            int index = findIndex(sorted, nums[i]);
-            res.add(0, index);
+            int count = getSum(bit, nums[i] - 1);
+            res.add(0, count);
+            update(bit, nums[i]);
         }
         return res;
     }
     
-    public int findIndex(List<Integer> sorted, int num) {
-        int left = 0, right = sorted.size() - 1;
-        while(left <= right) {
-            int mid = left + (right - left) / 2;
-            if(sorted.get(mid) >= num) {
-                right = mid - 1;
-            }
-            else {
-                left = mid + 1;
-            }
+    public int getSum(int[] bit, int i) {
+        int res = 0;
+        
+        while(i > 0) {
+            res += bit[i - 1];
+            i = i - (i & (-i));
         }
-        sorted.add(left, num);
-        return left;
+        return res;
+    }
+    
+    public void update(int[] bit, int i) {
+        while(i <= bit.length) {
+            bit[i - 1]++;
+            i = i + (i & (-i));
+        }
     }
 }
