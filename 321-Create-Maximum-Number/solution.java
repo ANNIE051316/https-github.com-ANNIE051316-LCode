4a96a7165a1e359c9e496da44767023867226692
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-07-31 17:02:02 -0700

    [321. Create Maximum Number][Accepted]committed by LeetCode Extension

---
+++ 321-Create-Maximum-Number/solution.java
@@ -0,0 +1,72 @@
+public class Solution {
+    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+        int[] res = new int[k];
+        
+        for(int i = Math.min(k, nums1.length); k - i <= nums2.length && i >= 0; i--) {
+            int[] n1 = maxArray(nums1, i);
+            int[] n2 = maxArray(nums2, k - i);
+            int[] tmp = merge(n1, n2);
+            for(int j = 0; j < k; j++) {
+                if(tmp[j] > res[j]) {
+                    res = tmp;
+                    break;
+                }
+                else if(tmp[j] < res[j]) {
+                    break;
+                }
+            }
+        }
+        
+        return res;
+    }
+    
+    public int[] maxArray(int[] nums, int k) {
+        int[] res = new int[k];
+        if(k == 0) {
+            return res;
+        }
+        
+        int pos = 0;
+        int i = 0;
+        int needed = k;
+        while(i < k) {
+            for(int j = pos + 1; j <= nums.length - needed; j++) {
+                if(nums[j] > nums[pos]) {
+                    pos = j;
+                }
+            }
+            needed--;
+            res[i++] = nums[pos++];
+        }
+        return res;
+    }
+    
+    public int[] merge(int[] num1, int[] num2) {
+        int[] res = new int[num1.length + num2.length];
+        int index = 0;
+        int i = 0, j = 0;
+        while(index < res.length) {
+            res[index++] = greater(num1, i, num2, j) ? num1[i++] : num2[j++];
+        }
+        return res;
+    }
+    
+    
+    public boolean greater(int[] num1, int i1, int[] num2, int i2) {
+        while(i1 < num1.length && i2 < num2.length) {
+            if(num1[i1] > num2[i2]) {
+                return true;
+            }
+            else if(num1[i1] < num2[i2]) {
+                return false;
+            }
+            i1++;
+            i2++;
+        }
+        
+        return i1 == num1.length ? false : true;
+    }
+    
+    
+    
+}
