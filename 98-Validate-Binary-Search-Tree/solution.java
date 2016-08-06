68a7aed9d701c47d58725f583e0d5c88bc2ae3ee
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-06 15:08:51 -0700

    [98. Validate Binary Search Tree][Accepted]committed by LeetCode Extension

--- 98-Validate-Binary-Search-Tree/solution.java
+++ 98-Validate-Binary-Search-Tree/solution.java
@@ -9,17 +9,17 @@
  */
 public class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isValid(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        return isValidHelper(root, Long.MIN_VALUE, Long.MAX_VALUE);   
     }
     
-    public boolean isValid(TreeNode root, long minValue, long maxValue) {
+    public boolean isValidHelper(TreeNode root, long minValue, long maxValue) {
         if(root == null) {
             return true;
         }
+        
         if(root.val <= minValue || root.val >= maxValue) {
             return false;
         }
-        
-        return isValid(root.left, minValue, root.val) && isValid(root.right, root.val, maxValue);
+        return isValidHelper(root.left, minValue, root.val) && isValidHelper(root.right, root.val, maxValue);
     }
 }
