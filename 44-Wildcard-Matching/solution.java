63f49abe1b6ef5b4b13d5160813778400f38cca1
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-06 15:01:32 -0700

    [44. Wildcard Matching][Accepted]committed by LeetCode Extension

--- 44-Wildcard-Matching/solution.java
+++ 44-Wildcard-Matching/solution.java
@@ -1,48 +1,37 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        if(s == null || s.length() == 0) {
-            if(p == null || p.length() == 0) {
-                return true;
+        int index = 0, pindex = 0, star = -1, mark = -1;
+        
+        while(index < s.length()) {
+            char c = s.charAt(index);
+           
+            if(pindex < p.length() && (p.charAt(pindex) == '?' || p.charAt(pindex) == c)) {
+                pindex++;
+                index++;
+            }
+            else if(pindex < p.length() && p.charAt(pindex) == '*') {
+                star = pindex;
+                mark = index;
+                pindex++;
+              //  index++;
+            }
+            else if(star != -1) {
+                pindex = star + 1;
+                mark++;
+                index = mark;
             }
             else {
-                for(int i = 0; i < p.length(); i++) {
-                    if(p.charAt(i) != '*') {
-                        return false;
-                    }
-                }
-            }
-            return true;
-        }
-        
-        if(p == null || p.length() == 0) {
-            return false;
-        }
-        
-        boolean[][] dp = new boolean[p.length() + 1][s.length() + 1];
-        
-        dp[0][0] = true;
-        for(int i = 0; i < p.length(); i++) {
-            if(p.charAt(i) == '*') {
-                dp[i + 1][0] = true; 
-            }
-            else {
-                break;
+                return false;
             }
         }
         
-        for(int i = 1; i < p.length() + 1; i++) {
-            char c = p.charAt(i - 1);
-            for(int j = 1; j < s.length() + 1; j++){
-                char t = s.charAt(j - 1);
-                if(c != '*') {
-                    dp[i][j] = dp[i - 1][j - 1] && ( c == '?' || c == t);
-                }
-                else {
-                    dp[i][j] = dp[i - 1][j - 1] || dp[i - 1][j] || dp[i][j - 1];
-                }
+        while(pindex < p.length()) {
+            if(p.charAt(pindex) != '*') {
+                return false;
             }
+            pindex++;
         }
         
-        return dp[dp.length - 1][dp[0].length - 1];
+        return true;
     }
 }
