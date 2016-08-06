814f6f990748100dbad0ac6064867c92258e380c
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-06 14:14:35 -0700

    [139. Word Break][Accepted]committed by LeetCode Extension

--- 139-Word-Break/solution.java
+++ 139-Word-Break/solution.java
@@ -3,27 +3,27 @@ public class Solution {
         if(s == null || s.length() == 0) {
             return true;
         }
-        
         if(wordDict == null || wordDict.size() == 0) {
             return false;
         }
+        
         boolean[] dp = new boolean[s.length() + 1];
-        dp[0] = true;
+        
         Set<Integer> lenset = new HashSet();
-
         for(String word : wordDict) {
             lenset.add(word.length());
         }
         
-        for(int i = 1; i < dp.length; i++) {
+        dp[0] = true;
+        for(int i = 0; i < s.length(); i++) {
             for(int len : lenset) {
-                if(i - len >= 0 && dp[i - len] && wordDict.contains(s.substring(i - len, i))) {
-                    dp[i] = true;
+                if(i - len + 1 >= 0 && dp[i - len + 1] && wordDict.contains(s.substring(i - len + 1, i + 1))) {
+                    dp[i + 1] = true;
                     break;
                 }
             }
         }
         
-        return dp[dp.length - 1];
-    }
+        return dp[s.length()];
+     }
 }
