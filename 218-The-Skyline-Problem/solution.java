2cc804ce8190d89c3637f4ca695bef2643399019
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-06 16:20:46 -0700

    [218. The Skyline Problem][Accepted]committed by LeetCode Extension

--- 218-The-Skyline-Problem/solution.java
+++ 218-The-Skyline-Problem/solution.java
@@ -1,68 +1,60 @@
-class Edge{
-    int x;
-    int height;
-    boolean isStart;
-    Edge(int x, int height, boolean isStart) {
-        this.x = x;
-        this.height = height;
-        this.isStart = isStart;
-    }
-}
-
 public class Solution {
+    class Point{
+        int index;
+        int height;
+        boolean isstart;
+        public Point(int index, int height, boolean isstart) {
+            this.index = index;
+            this.height = height;
+            this.isstart = isstart;
+        }
+    }
     public List<int[]> getSkyline(int[][] buildings) {
         List<int[]> res = new ArrayList();
         if(buildings == null || buildings.length == 0 || buildings[0].length == 0) {
             return res;
         }
         
-        List<Edge> edges = new ArrayList<Edge>();
-        
+        List<Point> points = new ArrayList();
         for(int[] building : buildings) {
-            edges.add(new Edge(building[0], building[2], true));
-            edges.add(new Edge(building[1], building[2], false));
+            points.add(new Point(building[0], building[2], true));
+            points.add(new Point(building[1], building[2], false));
         }
         
-        Collections.sort(edges, new Comparator<Edge>() {
+        Collections.sort(points, new Comparator<Point>() {
             @Override
-            public int compare(Edge e1, Edge e2) {
-                if(e1.x != e2.x) {
-                    return e1.x - e2.x;
+            public int compare(Point p1, Point p2) {
+                if(p1.index != p2.index) {
+                    return p1.index - p2.index;
                 }
-                else if(e1.isStart && e2.isStart) {
-                    return e2.height - e1.height;
+                else if(p1.isstart && p2.isstart) {
+                    return p2.height - p1.height;
                 }
-                else if(!e1.isStart && !e2.isStart) {
-                    return e1.height - e2.height;
+                else if(!p1.isstart && !p1.isstart) {
+                    return p1.height - p2.height;
                 }
                 else {
-                    return e1.isStart ? -1 : 1;
+                    return p1.isstart ? -1 : 1; 
                 }
             }
         });
-        
         PriorityQueue<Integer> height = new PriorityQueue<Integer>(11, Collections.reverseOrder());
-        
-        for(Edge edge : edges) {
-            if(edge.isStart) {
-                if(height.isEmpty() || height.peek() < edge.height) {
-                    res.add(new int[]{edge.x, edge.height});
+        height.offer(0);
+        for(Point point : points) {
+            if(point.isstart) {
+                if(point.height > height.peek()) {
+                    res.add(new int[]{point.index, point.height});
                 }
-                height.add(edge.height);
+                height.offer(point.height);
             }
             else {
-                height.remove(edge.height);
-                if(height.isEmpty()) {
-                    res.add(new int[]{edge.x, 0});
-                }
-                else if(height.peek() < edge.height){
-                    res.add(new int[]{edge.x, height.peek()});
+                height.remove(point.height);
+                if(point.height > height.peek()) {
+                    res.add(new int[]{point.index, height.peek()});
                 }
             }
         }
         
         return res;
-        
-        
     }
 }
