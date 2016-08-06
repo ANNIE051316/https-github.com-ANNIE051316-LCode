1f03274242b42ece42dd361aa18094e00df09082
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-06 15:40:49 -0700

    [1. Two Sum][Accepted]committed by LeetCode Extension

--- 1-Two-Sum/solution.java
+++ 1-Two-Sum/solution.java
@@ -4,11 +4,14 @@ public class Solution {
             throw new IllegalArgumentException("Input array has a length less than 2");
         }
         
-        for(int i = 0; i < nums.length - 1; i++) {
-            for(int j = i + 1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            else {
+                map.put(target - nums[i], i);
             }
         }
         
