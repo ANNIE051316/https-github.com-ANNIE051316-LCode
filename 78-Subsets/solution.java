d0b5c99f701f46be7b1fff04a16846cac22a263f
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-06 16:48:08 -0700

    [78. Subsets][Accepted]committed by LeetCode Extension

--- 78-Subsets/solution.java
+++ 78-Subsets/solution.java
@@ -1,18 +1,15 @@
 public class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        
-        backtrack(nums, 0, new ArrayList<Integer>(), res);
-        return res;
-    }
-    
-    public void backtrack(int[] nums, int start, List<Integer> path, List<List<Integer>> res) {
-        res.add(new ArrayList<Integer>(path));
-        
-        for(int i = start; i < nums.length; i++) {
-            path.add(nums[i]);
-            backtrack(nums, i + 1, path, res);
-            path.remove(path.size() - 1);
+        List<List<Integer>> res = new ArrayList();
+        res.add(new ArrayList());
+        for(int num : nums) {
+            int sz = res.size();
+            for(int i = 0; i < sz; i++) {
+                List<Integer> tmp = new ArrayList(res.get(i));
+                tmp.add(num);
+                res.add(tmp);
+            }
         }
+        return res;
     }
 }
