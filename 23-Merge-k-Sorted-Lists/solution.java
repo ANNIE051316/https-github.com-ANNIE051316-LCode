8e8f4608c66c86a2ebff5ed0c634db876e01ec78
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-07 16:09:56 -0700

    [23. Merge k Sorted Lists][Accepted]committed by LeetCode Extension

--- 23-Merge-k-Sorted-Lists/solution.java
+++ 23-Merge-k-Sorted-Lists/solution.java
@@ -12,32 +12,39 @@ public class Solution {
             return null;
         }
         
+        return partition(lists, 0, lists.length - 1);
+    }
+    
+    public ListNode partition(ListNode[] lists, int s, int e) {
+        if(s == e) {
+            return lists[s];
+        }
+        int mid = s + (e - s) / 2;
+        ListNode l1 = partition(lists, s, mid);
+        ListNode l2 = partition(lists, mid + 1, e);
         ListNode dummy = new ListNode(0);
-        
-        PriorityQueue<ListNode> pq = new PriorityQueue(11, new Comparator<ListNode>() {
-            @Override
-            
-            public int compare(ListNode n1, ListNode n2) {
-                return n1.val - n2.val;
-            }
-        });
-        
-        for(ListNode list : lists) {
-            if(list != null) {
-                pq.offer(list);
-            }
-        }
-        
         ListNode prev = dummy;
-        while(!pq.isEmpty()) {
-            ListNode min = pq.poll();
-            prev.next = min;
-            prev = prev.next;
-            if(pq.size() > 0 && min.next != null) {
-                pq.offer(min.next);
+        while(l1 != null && l2 != null) {
+            if(l1.val <= l2.val) {
+                prev.next = l1;
+                prev = prev.next;
+                l1 = l1.next;
+            }
+            else {
+                prev.next = l2;
+                prev = prev.next;
+                l2 = l2.next;
             }
         }
         
+        if(l1 != null) {
+            prev.next = l1;
+        }
+        if(l2 != null) {
+            prev.next = l2;
+        }
         return dummy.next;
     }
+    
+    
 }
