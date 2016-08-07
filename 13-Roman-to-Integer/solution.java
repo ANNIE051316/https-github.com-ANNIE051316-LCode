68306f477d0e6bc83a1e921f5ce613e59cc844e0
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-06 19:29:32 -0700

    [13. Roman to Integer][Compile Error]committed by LeetCode Extension

--- 13-Roman-to-Integer/solution.java
+++ 13-Roman-to-Integer/solution.java
@@ -3,76 +3,54 @@ public class Solution {
         if(s == null || s.length() == 0) {
             return 0;
         }
-        
         int res = 0;
-        int prev = 0;
+        if(s.indexOf("IV") != -1) {
+            res -= 2;
+        }
+        if(s.indexOf("IX") != -1) {
+            res -= 2;
+        }
+        if(s.indexOf("XL") != -1) {
+            res -= 20;
+        }
+        if(s.indexOf("XC") != -1) {
+            res -= 20;
+        }
+        if(s.indexOf("CD") != -1) {
+            res -= 200;
+        }
+        if(s.indexOf("CM") != 1) {
+            res -= 200;
+        }
+        
         for(int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             switch (c) {
-                case 'I':
-                    res += prev;
-                    prev = 1;
+                case "I":
+                    res += 1;
                     break;
-                case 'V':
-                    if(prev == 1) {
-                        prev = 4;
-                    }
-                    else {
-                        res += prev;
-                        prev = 5;
-                    }
+                case "V":
+                    res += 5;
                     break;
-                case 'X':
-                    if(prev == 1) {
-                        prev = 9;
-                    }
-                    else {
-                        res += prev;
-                        prev = 10;
-                    }
+                case "X":
+                    res += 10;
                     break;
-                case 'L':
-                    if(prev == 10) {
-                        prev = 40;
-                    }
-                    else {
-                        res += prev;
-                        prev = 50;
-                    }
+                case "L":
+                    res += 50;
                     break;
-                case 'C':
-                    if(prev == 10) {
-                        prev = 90;
-                    }
-                    else {
-                        res += prev;
-                        prev = 100;
-                    }
+                case "C":
+                    res += 100;
                     break;
-                case 'D':
-                    if(prev == 100) {
-                        prev = 400;
-                    }
-                    else {
-                        res += prev;
-                        prev = 500;
-                    }
+                case "D":
+                    res += 500;
                     break;
-                case 'M':
-                    if(prev == 100) {
-                        prev = 900;
-                    }
-                    else {
-                        res += prev;
-                        prev = 1000;
-                    }
+                case "M":
+                    res += 1000;
                     break;
                 default:
-                    break;
+                break;
             }
         }
-        
-        res += prev;
         return res;
     }
 }
