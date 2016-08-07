e527045c58df4d6422cf642f17f6fcc7f8629b02
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-06 23:12:16 -0700

    [25. Reverse Nodes in k-Group][Runtime Error]committed by LeetCode Extension

--- 25-Reverse-Nodes-in-k-Group/solution.java
+++ 25-Reverse-Nodes-in-k-Group/solution.java
@@ -11,23 +11,24 @@ public class Solution {
         if(head == null || k <= 1) {
             return head;
         }
-        
         ListNode dummy = new ListNode(0);
-        dummy.next = head;
         ListNode prev = dummy;
+        prev.next = head;
         ListNode p1 = head, p2 = head;
-        while(p2 != null) {
+        while(p2.next != null) {
             int count = 0;
             while(p2 != null && count < k) {
                 p2 = p2.next;
                 count++;
             }
+            
             if(count < k) {
                 break;
             }
+            
             while(p1.next != p2) {
                 ListNode tmp = p1.next;
-                p1.next = p1.next.next;
+                p1.next = tmp.next;
                 tmp.next = prev.next;
                 prev.next = tmp;
             }
@@ -36,6 +37,5 @@ public class Solution {
         }
         
         return dummy.next;
-        
     }
 }
