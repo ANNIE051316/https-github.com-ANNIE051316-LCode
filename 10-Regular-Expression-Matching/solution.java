7b00140fe6bbda771454ac6e6907aad25eb2b550
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-07 12:14:38 -0700

    [10. Regular Expression Matching][Accepted]committed by LeetCode Extension

--- 10-Regular-Expression-Matching/solution.java
+++ 10-Regular-Expression-Matching/solution.java
@@ -1,26 +1,34 @@
 public class Solution {
     public boolean isMatch(String s, String p) {
-        int slen = s.length();
         int plen = p.length();
-        boolean[][] dp = new boolean[slen + 1][plen + 1];
-        dp[0][0] = true;
-        for(int j = 1; j < plen && p.charAt(j) == '*'; j += 2) {
-            dp[0][j] = true;
-            dp[0][j + 1] = true;
+        if(plen == 0) {
+            return s.length() == 0;
         }
         
-        for(int i = 1; i <= slen; i++) {
-            char c = s.charAt(i - 1);
-            for(int j = 1; j <= plen; j++) {
-                char t = p.charAt(j - 1);
-                if(t != '*') {
-                    dp[i][j] = dp[i - 1][j - 1] && (t == '.' || t == c);
+        if(plen == 1) {
+            return (s.length() == 1 && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0)));
+        }
+        
+        if(p.charAt(1) == '*') {
+            if(isMatch(s, p.substring(2))) {
+                return true;
+            }
+            else {
+                if(s.length() > 0 && (p.charAt(0) == '.' || p.charAt(0) == s.charAt(0))) {
+                    return isMatch(s.substring(1), p);
                 }
                 else {
-                    dp[i][j] = dp[i][j - 2] || (dp[i - 1][j] && (c == p.charAt(j - 2) || p.charAt(j - 2) == '.'));
+                    return false;
                 }
             }
         }
-        return dp[slen][plen];
+        else {
+            if(s.length() > 0 && ( p.charAt(0) == '.' || p.charAt(0) == s.charAt(0))) {
+                return isMatch(s.substring(1), p.substring(1));
+            }
+            else {
+                return false;
+            }
+        }
     }
 }
