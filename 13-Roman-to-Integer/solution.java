b97a4919ca985cb4035850814deae270684643ef
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-06 19:16:25 -0700

    [13. Roman to Integer][Accepted]committed by LeetCode Extension

--- 13-Roman-to-Integer/solution.java
+++ 13-Roman-to-Integer/solution.java
@@ -1,66 +1,78 @@
 public class Solution {
     public int romanToInt(String s) {
-        int len = s.length();
+        if(s == null || s.length() == 0) {
+            return 0;
+        }
+        
         int res = 0;
-        int index = 0;
-        while(index < len) {
-            char c = s.charAt(index);
+        int prev = 0;
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
             switch (c) {
                 case 'I':
-                    if(index < len - 1 && s.charAt(index + 1) == 'V') {
-                        res += 4;
-                        index++;
-                    }
-                    else if(index < len - 1 && s.charAt(index + 1) == 'X') {
-                        res += 9;
-                        index++;
+                    res += prev;
+                    prev = 1;
+                    break;
+                case 'V':
+                    if(prev == 1) {
+                        prev = 4;
                     }
                     else {
-                        res += 1;
+                        res += prev;
+                        prev = 5;
                     }
-                    break;
-                case 'V': 
-                    res += 5;
                     break;
                 case 'X':
-                    if(index < len - 1 && s.charAt(index + 1) == 'L') {
-                        res += 40;
-                        index++;
-                    }
-                    else if(index < len - 1 && s.charAt(index + 1) == 'C') {
-                        res += 90;
-                        index++;
+                    if(prev == 1) {
+                        prev = 9;
                     }
                     else {
-                        res += 10;
+                        res += prev;
+                        prev = 10;
                     }
                     break;
-                case 'L': 
-                    res += 50;
+                case 'L':
+                    if(prev == 10) {
+                        prev = 40;
+                    }
+                    else {
+                        res += prev;
+                        prev = 50;
+                    }
                     break;
                 case 'C':
-                    if(index < len - 1 && s.charAt(index + 1) == 'D') {
-                        res += 400;
-                        index++;
-                    }
-                    else if(index < len - 1 && s.charAt(index + 1) == 'M') {
-                        res += 900;
-                        index++;
+                    if(prev == 10) {
+                        prev = 90;
                     }
                     else {
-                        res += 100;
+                        res += prev;
+                        prev = 100;
                     }
                     break;
                 case 'D':
-                    res += 500;
+                    if(prev == 100) {
+                        prev = 400;
+                    }
+                    else {
+                        res += prev;
+                        prev = 500;
+                    }
                     break;
                 case 'M':
-                    res += 1000;
+                    if(prev == 100) {
+                        prev = 900;
+                    }
+                    else {
+                        res += prev;
+                        prev = 1000;
+                    }
+                    break;
+                default:
                     break;
             }
-            index++;
         }
         
+        res += prev;
         return res;
     }
 }
