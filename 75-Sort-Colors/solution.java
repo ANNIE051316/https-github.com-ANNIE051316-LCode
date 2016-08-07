0f116df9479ebd4e7eca16aa826a641524318e2d
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-06 17:42:39 -0700

    [75. Sort Colors][Accepted]committed by LeetCode Extension

--- 75-Sort-Colors/solution.java
+++ 75-Sort-Colors/solution.java
@@ -3,32 +3,27 @@ public class Solution {
         if(nums == null || nums.length == 0) {
             return;
         }
-        int end0 = -1, start2 = nums.length;
-        while(start2 - 1 >= 0 && nums[start2 - 1] == 2) {
-            start2--;
-        }
-        int i = 0;
-        while(i < start2){
-            if(nums[i] == 0) {
-                if(i == end0 + 1) {
-                    i++;
-                }
-                else {
-                    nums[i] = nums[end0 + 1];
-                    nums[end0 + 1] = 0;
-                }
-                end0 += 1;
+        int left = -1, right = nums.length;
+        
+        for(int i = 0; i < right; i++) {
+            if(nums[i] == 1) {
+                continue;
             }
-            else if(nums[i] == 2 && i != start2 - 1) {
-                nums[i] = nums[start2 - 1];
-                nums[start2 - 1] = 2;
-                while(start2 - 1 >= 0 && nums[start2 - 2] == 2) {
-                    start2--;
+            else if(nums[i] == 0){
+                left++;
+                if(i != left) {
+                    nums[i] = nums[left];
+                    nums[left] = 0;
+                    i--;
                 }
             }
             else {
-                i++;
+                right--;
+                nums[i] = nums[right];
+                nums[right] = 2;
+                i--;
             }
         }
+        
     }
 }
