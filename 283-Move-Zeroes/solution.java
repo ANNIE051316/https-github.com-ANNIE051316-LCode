53978f09a4eb6caf3ab73cafc969b9c2abf6e54a
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-07 15:02:00 -0700

    [283. Move Zeroes][Accepted]committed by LeetCode Extension

--- 283-Move-Zeroes/solution.java
+++ 283-Move-Zeroes/solution.java
@@ -1,14 +1,18 @@
 public class Solution {
     public void moveZeroes(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return;
+        }
+        
         int prev = -1;
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] != 0) {
-                nums[++prev] = nums[i];
+                if(++prev == i) {
+                    continue;
+                }
+                nums[prev] = nums[i];
+                nums[i] = 0;
             }
-        }
-        
-        for(int i = prev + 1; i < nums.length; i++) {
-            nums[i] = 0;
         }
     }
 }
