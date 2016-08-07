c2bed39050c74f1b0ac46753a96944953d975dfd
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-06 17:58:06 -0700

    [71. Simplify Path][Accepted]committed by LeetCode Extension

--- 71-Simplify-Path/solution.java
+++ 71-Simplify-Path/solution.java
@@ -1,33 +1,35 @@
 public class Solution {
     public String simplifyPath(String path) {
-        String[] strs = path.split("/");
-        Stack<String> stack = new Stack<String>();
-        
-        for(int i = 0; i < strs.length; i++) {
-            switch (strs[i]) {
-                case ".":
-                    break;
-                case "..":
-                    if(!stack.isEmpty()) {
-                        stack.pop();
-                    }
-                    break;
-                case "":
-                    break;
-                default:
-                    stack.push(strs[i]);
-            }
+        if(path == null || path.length() == 0) {
+            return "/";
         }
         
+        String[] strs = path.split("\\/");
+        Stack<String> stack = new Stack();
+        for(String s : strs) {
+            s = s.trim();
+            if(s.equals(".") || s.equals("")) {
+                continue;
+            }
+            else if(s.equals("..")) {
+                if(!stack.isEmpty()) {
+                    stack.pop();
+                }
+            }
+            else {
+                stack.push(s);
+            }
+        }
         StringBuilder sb = new StringBuilder();
         while(!stack.isEmpty()) {
-            if(sb.length() != 0) {
-                sb.insert(0, '/');
+            if(sb.length() > 0) {
+                sb.insert(0, stack.pop() + "/");
             }
-            sb.insert(0, stack.pop());
+            else {
+                sb.insert(0, stack.pop());
+            }
         }
-        
-        sb.insert(0, '/');
+        sb.insert(0, "/");
         return sb.toString();
     }
 }
