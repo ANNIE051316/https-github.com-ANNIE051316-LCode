3292bd8e4f21876fa695de1cb608126032033465
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-07 18:13:06 -0700

    [273. Integer to English Words][Accepted]committed by LeetCode Extension

--- 273-Integer-to-English-Words/solution.java
+++ 273-Integer-to-English-Words/solution.java
@@ -1,38 +1,53 @@
 public class Solution {
     public String numberToWords(int num) {
-        String[] thousands = new String[]{"", "Thousand", "Million", "Billion"};
-        String[] tens = new String[]{"", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-        String[] ones = new String[]{"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        if(num < 0) {
+            throw new IllegalArgumentException("Input is negative " + num);
+        }
         
-        int i = 0; 
+        String[] lessthan20 = new String[]{"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+        String[] tens = new String[]{"","Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+        String[] thousands = new String[]{"", "Thousand", "Million", "Billion", "Trillion"};
+        
         StringBuilder sb = new StringBuilder();
+        int thindex = 0;
         
-        while(num != 0)  {
-            if(num % 1000 != 0) {
-                String tmpsb = helper(num % 1000, tens, ones);
-                tmpsb = tmpsb + thousands[i] + " ";
-                sb.insert(0, tmpsb);
+        while(num != 0) {
+            StringBuilder tmp = new StringBuilder();
+            int curnum = num % 1000;
+            if(curnum / 100 != 0) {
+                tmp.append(lessthan20[curnum / 100] + " Hundred");
             }
+            curnum %= 100;
             
+            if(curnum < 20 ) {
+                if(tmp.length() > 0 && curnum != 0) {
+                    tmp.append(" ");
+                }
+                tmp.append(lessthan20[curnum]);
+            }
+            else {
+                if(tmp.length() > 0) {
+                    tmp.append(" ");
+                }
+                
+                tmp.append(tens[curnum / 10]);
+                if(curnum % 10 > 0) {
+                    tmp.append(" " + lessthan20[curnum % 10]);
+                } 
+            }
+            if(tmp.length() > 0 && thindex > 0) {
+                tmp.append(" " + thousands[thindex]);    
+            }
+            if(tmp.length() > 0) {
+                if(sb.length() > 0) {
+                    tmp.append(" ");
+                }
+                sb.insert(0, tmp);
+            }
             num /= 1000;
-            i++;
+            thindex++;
         }
         
-        return sb.length() == 0 ? "Zero" : sb.toString().trim();
-    }
-    
-    public String helper(int num, String[] tens, String[] ones) {
-        if(num == 0) {
-            return "";
-        }
-        else if(num < 20) {
-            return ones[num] + " ";
-        }
-        else if(num < 100){
-            return tens[num / 10] + " " + helper(num % 10, tens, ones);
-        }
-        else {
-            return ones[num / 100] + " Hundred " + helper(num % 100, tens, ones);
-        }
-    } 
+        return sb.length() == 0 ? "Zero" : sb.toString();
+     }
 }
