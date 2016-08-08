0c286c84e90c01a221e9daa846a08beedbf38fd1
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-07 23:50:34 -0700

    [278. First Bad Version][Accepted]committed by LeetCode Extension

---
+++ 278-First-Bad-Version/solution.java
@@ -0,0 +1,24 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        if(n < 1) {
+            throw new IllegalArgumentException("");
+        }
+        
+        int left = 1, right = n;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            boolean isbad = isBadVersion(mid);
+            if(isbad) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+}
