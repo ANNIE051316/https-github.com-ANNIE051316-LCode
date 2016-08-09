c681a1bb8268bc6fd368162b82315f22fa279678
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-08 23:27:27 -0700

    [133. Clone Graph][Runtime Error]committed by LeetCode Extension

---
+++ 133-Clone-Graph/solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     List<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        Map<UndirectedGraphNode, UndirectedGraphNode> map = new HashMap();
+        Queue<UndirectedGraphNode> queue = new LinkedList();
+        queue.offer(node);
+        map.put(node, new UndirectedGraphNode(node.label));
+        while(!queue.isEmpty()) {
+            UndirectedGraphNode cur = queue.poll();
+            for(UndirectedGraphNode neighbor : cur.neighbors) {
+                if(!map.containsKey(neighbor)) {
+                    map.put(neighbor, new UndirectedGraphNode(neighbor.label));
+                    queue.offer(neighbor);
+                }
+                map.get(cur).neighbors.add(map.get(neighbor));
+            }
+        }
+        
+        return map.get(node);
+    }
+}
