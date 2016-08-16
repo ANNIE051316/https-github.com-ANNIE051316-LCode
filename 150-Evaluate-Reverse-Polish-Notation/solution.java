27cae83993f233df132d481b1f136a739bfc85b0
ANNIE051316 <ANNIE051316@users.noreply.github.com>
2016-08-15 23:00:09 -0700

    [150. Evaluate Reverse Polish Notation][Accepted]committed by LeetCode Extension

--- 150-Evaluate-Reverse-Polish-Notation/solution.java
+++ 150-Evaluate-Reverse-Polish-Notation/solution.java
@@ -1,26 +1,43 @@
 public class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stack = new Stack();
-        for(int i = 0; i < tokens.length; i++) {
-            switch (tokens[i]) {
-                case "+":
-                    stack.push(stack.pop() + stack.pop());
-                    break;
-                case "-":
-                    stack.push(-stack.pop() + stack.pop());
-                    break;
-                case "*":
-                    stack.push(stack.pop() * stack.pop());
-                    break;
-                case "/":
-                    stack.push(1/stack.pop() * stack.pop());
-                    break;
-                default:
-                    stack.push(Integer.parseInt(tokens[i]));
-                    break;
+        if(tokens == null || tokens.length == 0) {
+            return 0;
+        }
+        
+        Stack<Integer>  stack = new Stack();
+        
+        for(int i  = 0; i < tokens.length; i++) {
+            if(tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*") || tokens[i].equals("/")) {
+                if(stack.size() < 2) {
+                    return 0;
+                }
+                int oper2 = stack.pop();
+                int oper1 = stack.pop();
+                switch(tokens[i]) {
+                    case "+":
+                        stack.push(oper1 + oper2);
+                        break;
+                    case "-":
+                        stack.push(oper1 - oper2);
+                        break;
+                    case "*":
+                        stack.push(oper1 * oper2);
+                        break;
+                    case "/":
+                        if(oper2 == 0) {
+                            return Integer.MAX_VALUE;
+                        }
+                        stack.push(oper1 / oper2);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else {
+                stack.push(Integer.parseInt(tokens[i]));
             }
         }
         
-        return stack.pop();
+        return stack.size() == 1 ? stack.pop() : 0;
     }
 }
